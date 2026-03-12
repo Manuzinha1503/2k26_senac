61f59008945c925956603a7db11edf997daeb305
EMANUELLI RODRIGUES BRITZ <04489986025@senacrs.edu.br>
2026-03-12 12:22:43 -0300

    final d aula

--- node/ex2.js
+++ node/ex2.js
@@ -5,7 +5,7 @@ let n3 = Number(prompt("Numero 3: "));
 if(n1 == n2 && n1 == n3){
     console.log("Triangulo equilatero")
 }
-else if((a == c) || (a == b) && (b == c)){
+else if((n1 == n2) || (n1 == n3) && (n3 == n2)){
     console.log("Triângulo issoseles")
 }
 else if(n3 + n1 > n2 && n3 + n1 > n2 && n1 + n2 > n3 ){
